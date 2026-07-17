f27f539f2bcbaa7fce962af9cba4ab27fef11a20
unknown <savcenkosolomia94@gmail.com>
2026-07-17 19:04:24 +0200

    hw23

--- lesson3/script.js
+++
@@ -1,4 +0,0 @@
-const name = "Jane Doe";
-let age = 26;
-const subject = "Develop";
-console.log(`name: ${name}, age in 5 years: ${age + 5}, subject: ${subject}`);
--- lesson8/django/mysite/mysite/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
-
-def about(request):
-    return HttpResponse("<h2>Do you know about Django?</h2>")
